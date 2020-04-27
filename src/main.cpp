27d11d0dddeff33b1fc5eb1405bd74053228a28d
Mitch Dart <mitch@sticitt.co.za>
2020-04-27 17:35:36 +0200

    Initial setup for standalone imgui

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "SharedCockpitConfig.h"
+
+int main(int argc, char** argv) {
+	std::cout << "SharedCockpit Version " << SharedCockpit_VERSION_MAJOR << "." << SharedCockpit_VERSION_MINOR << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
