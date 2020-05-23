9394be906fe72ed22691246e1a567b61af04be1c
Mitch <mitch@sticitt.co.za>
2020-05-23 12:39:35 +0200

    Remove git submodules where possible and use vcpkg

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-cd libs
-
-echo "Building RxCPP..."
-cd RxCpp
-mkdir build
 cd build
-cmake ..
-make install
-cd ../..
+cmake -G Ninja  -DCMAKE_BUILD_TYPE=Release -DProtobuf_BUILD_SHARED_LIBS=OFF -DProtobuf_USE_STATIC_LIBS=ON -DGAMENETWORKINGSOCKETS_BUILD_TESTS=OFF -DCMAKE_TOOLCHAIN_FILE=/Users/mitch/Sticitt/OpenSource/vcpkg/scripts/buildsystems/vcpkg.cmake ..
+ninja
