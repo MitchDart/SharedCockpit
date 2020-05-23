4fa7a9cc8d90ae38bf9426905344ad18d9e55098
Mitch <mitch@sticitt.co.za>
2020-05-23 12:53:56 +0200

    Add compiler flags to top level cmake

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd build
-cmake -G Ninja  -DCMAKE_BUILD_TYPE=Release -DProtobuf_BUILD_SHARED_LIBS=OFF -DProtobuf_USE_STATIC_LIBS=ON -DGAMENETWORKINGSOCKETS_BUILD_TESTS=OFF -DCMAKE_TOOLCHAIN_FILE=/Users/mitch/Sticitt/OpenSource/vcpkg/scripts/buildsystems/vcpkg.cmake ..
+cmake -G Ninja  -DCMAKE_TOOLCHAIN_FILE=/Users/mitch/Sticitt/OpenSource/vcpkg/scripts/buildsystems/vcpkg.cmake ..
 ninja
